08c6114d1527b5640fe797d2e29cbd4fc342f384
Sai Sandeep Rayanuthala <raynuthalas@gmail.com>
2021-04-07 00:45:23 +0530

    added cors

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/raysandeep/Agora-Cloud-Recording-Example/api"
 
 	"github.com/gofiber/fiber/v2"
@@ -26,6 +27,7 @@ func main() {
 	viper.AutomaticEnv()
 
 	app := fiber.New()
+	app.Use(cors.New())
 	app.Get("/", healthCheck)
 	api.MountRoutes(app)
 
